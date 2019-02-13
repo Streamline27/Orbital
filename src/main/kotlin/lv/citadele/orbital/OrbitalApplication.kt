71eb22475caab7f5bf12c4f3a20a99f9f1768516
Vladislav Yakushin <vladislavs.jakusins@citadele.lv>
2019-02-13 22:12:23 +0200

    initial commit

---
+++ src/main/kotlin/lv/citadele/orbital/OrbitalApplication.kt
@@ -0,0 +1,12 @@
+package lv.citadele.orbital
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrbitalApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrbitalApplication>(*args)
+}
+
